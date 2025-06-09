8e6711cbcf5f265c629f2590a474306884bc474b
sumithada18 <sumit_hada@epam.com>
2025-06-10 01:50:39 +0530

    Update std_function.cpp

--- callbacks/std_function.cpp
+++ callbacks/std_function.cpp
@@ -1,3 +1,13 @@
+/*
+=> One Type to Rule Them All: std::function lets you create a function parameter (or variable) that can accept any kind of callable 
+   (lambda, functor, function pointer) as long as its signature matches.
+=> Uniform Interface: It hides the original, specific type of the callable. The code receiving the std::function only cares that it 
+   can be called with the expected arguments and return type.
+=> Flexible API Design: This makes it easy to design APIs (like registerCallback(std::function<void(int)> cb)) that are generic and 
+   don't force users to provide callbacks in only one specific way.
+=> Storing Diverse Callables: You can store different kinds of callables (e.g., in a std::vector<std::function<...>>) if they share 
+   the same signature, which you can't easily do with their original distinct types.
+*/
 #include <iostream>
 #include <functional>
 #include <vector>
